a613b41bcf1214bfa2e15ba618f212ba983b5690
alex <alex@tekartik.com>
2020-02-15 17:40:15 +0100

    fix: lint and travis setup

---
+++ tutorial_todo/tool/ci.dart
@@ -0,0 +1,12 @@
+import 'package:process_run/shell.dart';
+
+Future main() async {
+  var shell = Shell();
+
+  await shell.run('''
+# Analyze code
+dartanalyzer --fatal-warnings --fatal-infos .
+dartfmt -n --set-exit-if-changed .
+pub global run webdev build
+''');
+}
--- tutorial_todo/web/todo.dart
+++ tutorial_todo/web/todo.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the COPYING file.
 
-// This is a port of "A Simple ToDo List Using HTML5 IndexedDB" to Dart.
+// This is a port of 'A Simple ToDo List Using HTML5 IndexedDB' to Dart.
 // See: http://www.html5rocks.com/en/tutorials/indexeddb/todo/
 
 import 'dart:html';
@@ -15,17 +15,17 @@ import 'dart:async';
 idb.IdbFactory idbFactory;
 
 class TodoList {
-  static final String _TODOS_DB = "com.tekartik.idb.todo";
-  static final String _TODOS_STORE = "todos";
+  static final String _TODOS_DB = 'com.tekartik.idb.todo';
+  static final String _todosStore = 'todos';
 
   idb.Database _db;
-  int _version = 2;
+  final _version = 2;
   InputElement _input;
   Element _todoItems;
 
   TodoList() {
     _todoItems = querySelector('#todo-items');
-    _input = querySelector('#todo');
+    _input = querySelector('#todo') as InputElement;
     querySelector('input#submit').onClick.listen((e) => _onAddTodo());
   }
 
@@ -50,35 +50,35 @@ class TodoList {
   }
 
   void _onUpgradeNeeded(idb.VersionChangeEvent e) {
-    idb.Database db = (e.target as idb.OpenDBRequest).result;
-    if (!db.objectStoreNames.contains(_TODOS_STORE)) {
-      db.createObjectStore(_TODOS_STORE, keyPath: 'timeStamp');
+    var db = (e.target as idb.OpenDBRequest).result;
+    if (!db.objectStoreNames.contains(_todosStore)) {
+      db.createObjectStore(_todosStore, keyPath: 'timeStamp');
     }
   }
 
   void _onAddTodo() {
     var value = _input.value.trim();
-    if (value.length > 0) {
+    if (value.isNotEmpty) {
       _addTodo(value);
     }
     _input.value = '';
   }
 
   Future _addTodo(String text) {
-    var trans = _db.transaction(_TODOS_STORE, 'readwrite');
-    var store = trans.objectStore(_TODOS_STORE);
+    var trans = _db.transaction(_todosStore, 'readwrite');
+    var store = trans.objectStore(_todosStore);
     return store
         .put({
           'text': text,
-          'timeStamp': new DateTime.now().millisecondsSinceEpoch.toString()
+          'timeStamp': DateTime.now().millisecondsSinceEpoch.toString()
         })
         .then((_) => _getAllTodoItems())
         .catchError((e) => _onError);
   }
 
   void _deleteTodo(String id) {
-    var trans = _db.transaction(_TODOS_STORE, 'readwrite');
-    var store = trans.objectStore(_TODOS_STORE);
+    var trans = _db.transaction(_todosStore, 'readwrite');
+    var store = trans.objectStore(_todosStore);
     var request = store.delete(id);
     request.then((e) => _getAllTodoItems(), onError: _onError);
   }
@@ -86,46 +86,45 @@ class TodoList {
   void _getAllTodoItems() {
     _todoItems.nodes.clear();
 
-    var trans = _db.transaction(_TODOS_STORE, 'readwrite');
-    var store = trans.objectStore(_TODOS_STORE);
+    var trans = _db.transaction(_todosStore, 'readwrite');
+    var store = trans.objectStore(_todosStore);
 
     // Get everything in the store.
     store.openCursor(autoAdvance: true).listen((cursor) {
-      _renderTodo(cursor.value);
+      _renderTodo(cursor.value as Map);
     }, onError: _onError);
   }
 
   void _renderTodo(Map todoItem) {
-    var textDisplay = new Element.tag('span');
-    textDisplay.text = todoItem['text'];
+    var textDisplay = Element.tag('span');
+    textDisplay.text = todoItem['text']?.toString();
 
-    var deleteControl = new Element.tag('a');
+    var deleteControl = Element.tag('a');
     deleteControl.text = '[Delete]';
-    deleteControl.onClick.listen((e) => _deleteTodo(todoItem['timeStamp']));
+    deleteControl.onClick
+        .listen((e) => _deleteTodo(todoItem['timeStamp']?.toString()));
 
-    var item = new Element.tag('li');
+    var item = Element.tag('li');
     item.nodes.add(textDisplay);
     item.nodes.add(deleteControl);
     _todoItems.nodes.add(item);
   }
 }
 
-/**
- * Typically the argument is window.location.search
- */
+/// Typically the argument is window.location.search
 Map<String, String> getArguments(String search) {
-  Map<String, String> params = new Map();
+  var params = <String, String>{};
   if (search != null) {
-    int questionMarkIndex = search.indexOf('?');
+    var questionMarkIndex = search.indexOf('?');
     if (questionMarkIndex != -1) {
       search = search.substring(questionMarkIndex + 1);
     }
-    search.split("&").forEach((e) {
-      if (e.contains("=")) {
-        List<String> split = e.split("=");
+    search.split('&').forEach((e) {
+      if (e.contains('=')) {
+        var split = e.split('=');
         params[split[0]] = split[1];
       } else {
-        if (!e.isEmpty) {
+        if (e.isNotEmpty) {
           params[e] = '';
         }
       }
@@ -135,15 +134,15 @@ Map<String, String> getArguments(String search) {
 }
 
 void main() {
-  var urlArgs = getArguments(window.location.search);
-  String idbFactoryName = urlArgs['idb_factory'];
+  var urlArgs = Uri.tryParse(window.location.search).queryParameters;
+  var idbFactoryName = urlArgs['idb_factory'];
   // init factory from url
   idbFactory = idb.getIdbFactory(idbFactoryName);
   if (idbFactory == null) {
     window.alert(
-        "No idbFactory of type '$idbFactoryName' supported on this browser");
+        'No idbFactory of type \'$idbFactoryName\' supported on this browser');
   } else {
-    querySelector("#idb span").innerHtml = "Using '${idbFactory.name}'";
-    new TodoList().open();
+    querySelector('#idb span').innerHtml = 'Using \'${idbFactory.name}\'';
+    TodoList().open();
   }
 }
